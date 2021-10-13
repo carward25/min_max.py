a5453cd04418bf63fc743fdfcf003139d0d1645f
carward25 <83989145+carward25@users.noreply.github.com>
2021-10-13 14:43:42 -0700

    Update min_max.py

--- min_max.py
+++ min_max.py
@@ -1,12 +1,12 @@
 #name: Cassidy Ward
 #date: 10/12/2021
-#description: This program asks the user to enter how many numbers they'd like to input, then asks the user to enter
-# each desired number. The program then gives the min and max values of the user input
+#description: This program asks the user to enter how many numbers they'd like to input, then asks the user to enter each desired number. The program then gives the min and max values of the user input
 
 num_1 = int(input("How many integers would you like to enter?"))
 print("Please enter", num_1, "integers.")
 _min = int(input())
 _max = _min
+for i in range(1, num_1):
     number = int(input())  
     if number > _max:
         _max = number
